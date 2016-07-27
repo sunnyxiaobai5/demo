83ae275110862347bc8af39cd343d53f3a91bb21
sunnyxiaobai5 <sunnyxiaobai5@gmail.com>
2016-07-28 01:23:58 +0800

    init

---
+++ portal/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%--
+  ~ Copyright (c) 2005, 2014 www.yineng.com
+  --%>
+
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
---
+++ repository/src/main/java/sunnyxiaobai5/Application.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * sunnyxiaobai5@gmail.com
+ * <p>
+ * <li>项目名称: demo</li>
+ * <li>完整包名: sunnyxiaobai5</li>
+ * <li>文件名称: Application.java</li>
+ * <li>内容摘要: </li>
+ * <li>内容描述: </li>
+ * <li>其他说明: </li>
+ * <li>@author Xiangyong Zeng</li>
+ ******************************************************************************/
+package sunnyxiaobai5;
+
+public class Application {
+}
